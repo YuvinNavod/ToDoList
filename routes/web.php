88c1e0799fdc070fe7bc3a389c2ea5de307c2d3e
Yuvin Navod <137191227+YuvinNavod@users.noreply.github.com>
2024-08-21 13:38:40 +0530

    up

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route :: prefix('/todo')->group(function(){
 
 });
 
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
